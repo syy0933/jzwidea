fce4ef09c94ef0c0227dfa07a5b96716ca1372af
jzw <jzw_0933@163.com>
2019-10-12 23:51:17 +0800

    提交一个工程

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+/**
+ * 包名:com.itheima.pojo
+ * 作者:金智伟
+ * 日期2019-10-12  23:49
+ */
+public class User {
+    private  String  username;
+    private  String password;
+    private  Integer id;
+
+}
