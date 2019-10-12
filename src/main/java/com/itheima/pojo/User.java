175e19b8c00f47692045367065a3ff9341aafff9
jzw <jzw_0933@163.com>
2019-10-13 00:09:15 +0800

    减少了字段

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -7,7 +7,7 @@ package com.itheima.pojo;
  */
 public class User {
     private  String  username;
-    private  String password;
+    private  String password;//////////
     private  Integer id;//用户id
 
 }
