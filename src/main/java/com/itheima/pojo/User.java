e01936ff3f0c5253ce1b48a5e9525fbacceeeda5
jzw <jzw_0933@163.com>
2019-12-15 19:07:09 +0800

    减少了main方法

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -12,9 +12,7 @@ public class User {
     private  Integer tid;//用户id////////
     private int idt;
 
-    public static void main(String[] args) {
 
-    }
     private int  ids;
     private int  ir;
     private int  irs1;
